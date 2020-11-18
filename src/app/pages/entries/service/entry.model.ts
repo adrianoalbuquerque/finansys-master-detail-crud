42c3e8e061852e1dc4efdb8cfc71d6373c7055e9
Adriano Teixeira de Albuquerque <ateixeira@procenge.com.br>
2020-11-18 18:56:22 -0300

    Adicione componente EntryList

--- src/app/pages/entries/service/entry.model.ts
+++ src/app/pages/entries/service/entry.model.ts
@@ -2,15 +2,15 @@ import { Category } from '../../categories/service/category.model';
 
 export class Entry {
   constructor(
-    public id: number,
-    public name: string,
-    public description: string,
-    public type: string,
-    public amount: string,
-    public date: string,
-    public paid: string,
-    public categoryId: number,
-    public category: Category
+    public id?: number,
+    public name?: string,
+    public description?: string,
+    public type?: string,
+    public amount?: string,
+    public date?: string,
+    public paid?: boolean,
+    public categoryId?: number,
+    public category?: Category
   ){}
 
   static types = {
