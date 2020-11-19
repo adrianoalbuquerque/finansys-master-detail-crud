1f74250519c2dc2d6c8111e20bd91851beb86e18
Adriano Teixeira de Albuquerque <ateixeira@procenge.com.br>
2020-11-19 11:43:04 -0300

    Adicione associação entre Entry e Category

--- src/app/pages/categories/service/category.model.ts
+++ src/app/pages/categories/service/category.model.ts
@@ -1,6 +1,6 @@
 export class Category {
   constructor() {}
-  id?: number;
-  name?: string;
-  description?: string;
+  public id?: number;
+  public name?: string;
+  public description?: string;
 }
--- src/app/pages/entries/service/entry.service.ts
+++ src/app/pages/entries/service/entry.service.ts
@@ -3,44 +3,59 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs/';
 import { map, catchError, flatMap } from 'rxjs/operators';
+import { CategoryService } from '../../categories/service/category.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EntryService {
-
   private apiPath = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private categoryService: CategoryService
+  ) {}
 
   getAll(): Observable<Entry[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntries)
-    );
+    return this.http
+      .get(this.apiPath)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntries));
   }
 
   getById(id: number): Observable<Entry> {
     const url = `${this.apiPath}/${id}`;
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
+    return this.http
+      .get(url)
+      .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+    // Configuração feita para resover problemas da apiFake
+    return this.categoryService.getById(entry.id).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        // Observable<Entry>
+        return this.http
+          .post(this.apiPath, entry)
+          .pipe(catchError(this.handleError), map(this.jsonDataToEntry));
+      })
     );
   }
 
   update(entry: any): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
 
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+     // Configuração feita para resover problemas da apiFake
+    return this.categoryService.getById(entry.id).pipe(
+      flatMap((category) => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
   }
 
@@ -57,7 +72,7 @@ export class EntryService {
   private jsonDataToEntries(jsonData: any[]): Entry[] {
     const entries: Entry[] = [];
 
-    jsonData.forEach(element => {
+    jsonData.forEach((element) => {
       const entry = Object.assign(new Entry(), element);
       entries.push(entry);
     });
