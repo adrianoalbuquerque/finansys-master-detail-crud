a53c4c1c94d4d2b14ad0af6220150cfeb7d69436
Adriano Teixeira de Albuquerque <ateixeira@procenge.com.br>
2020-11-18 21:37:06 -0300

    Melhore o componente EntryForm

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,17 +1,22 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EntriesRoutingModule } from './entries-routing.module';
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 
 @NgModule({
   declarations: [
-    EntryListComponent
+    EntryListComponent,
+    EntryFormComponent
   ],
   imports: [
     CommonModule,
-    EntriesRoutingModule
+    EntriesRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class EntriesModule { }
