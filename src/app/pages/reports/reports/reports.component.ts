89e855a855f7600f83ccec969eb9f7de79482aac
Adriano Teixeira de Albuquerque <ateixeira@procenge.com.br>
2020-11-20 18:54:34 -0300

    Adicione template de reports

--- src/app/pages/reports/reports/reports.component.ts
+++ src/app/pages/reports/reports/reports.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
+  months = [
+    {name: 'Selecione um mês', value: 0},
+    {name: 'Janeiro', value: 1},
+    {name: 'Fevereiro', value: 2},
+    {name: 'Março', value: 3},
+    {name: 'Abril', value: 4},
+    {name: 'Maio', value: 5},
+    {name: 'Junho', value: 6},
+    {name: 'Julho', value: 7},
+    {name: 'Agosto', value: 8},
+    {name: 'Setembro', value: 9},
+    {name: 'Outubro', value: 10},
+    {name: 'Novembro', value: 11},
+    {name: 'Dezembro', value: 12},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  generateReports(): void {
+
   }
 
 }
