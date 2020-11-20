b908b8c650b3d485acb1cf4d32b2dc2ac186c911
Adriano Teixeira de Albuquerque <ateixeira@procenge.com.br>
2020-11-20 19:00:36 -0300

    Melhore o componente PageHeader

--- src/app/shared/components/page-header/page-header.component.ts
+++ src/app/shared/components/page-header/page-header.component.ts
@@ -8,6 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   @Input('page-title') pageTitle: string;
+  @Input('show-button') showButton = true;
   @Input('button-class') buttonClass: string;
   @Input('button-text') butttonText: string;
   @Input('button-link') buttonLink: string;
